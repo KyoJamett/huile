e9e4b0d947e1ea7bece35cf17a728594e784c0fd
Ignacio <kyo.akamatsu@gmail.com>
2025-09-20 15:30:23 -0300

    feat: se reemplaza cdn de bootstrap por instalacion de dependencias bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./styles/bootstrap.scss";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
