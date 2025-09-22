aeab2da8eadbd4c5e0fc998e11df13dc5e052046
Ignacio <kyo.akamatsu@gmail.com>
2025-09-21 22:06:15 -0300

    feat: se instala font awesome para iconos de plantilla

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+// import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./styles/bootstrap.scss";
 import "./index.css";
 
